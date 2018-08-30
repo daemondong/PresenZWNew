9aa4f9548f30a360031335f17dddbb3c7e67cad4
daemondong <daemondong@163.com>
2018-08-30 09:32:17 +0800

    New 6_81_02

---
+++ SDK_v6_81_02/ProductPlus/SerialAPIPlus/app_version.h
@@ -0,0 +1,13 @@
+/**
+ * @file
+ * Contains version definitions which are used in the protocol.
+ */
+#ifndef PRODUCTPLUS_SERIALAPI_APP_VERSION_H_
+#define PRODUCTPLUS_SERIALAPI_APP_VERSION_H_
+
+#include "zaf_version.h"
+
+#define APP_VERSION 6
+#define APP_REVISION 1
+
+#endif /* PRODUCTPLUS_SERIALAPI_APP_VERSION_H_ */
